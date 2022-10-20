a0f9d884c0a9ce07f06f83fdee0540a46fdf062e
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-10-20 15:24:31 +0700

    push detail menu picture and infrastructure

--- lib/views/Detail_Menu.dart
+++ lib/views/Detail_Menu.dart
@@ -12,8 +12,29 @@ class _DetailMenuPageState extends State<DetailMenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [],
+        body: Stack(
+      children: [
+        Column(children: [
+          Flexible(
+              flex: 3,
+              child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                image: AssetImage('assets/images/detail_bg.png'),
+              )))),
+          //kosong
+          Flexible(flex: 5, child: Container())
+        ]),
+        Column(children: [
+          //kosong
+          Flexible(flex: 3, child: Container()),
+          //card
+          Flexible(flex: 6, child: Container(child: Text("Text")))
+        ]),
+      ],
     ));
   }
 }
