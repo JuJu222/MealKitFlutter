0355e21b16a90d73807cb0b11b668c183bb83169
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-10-24 12:36:13 +0700

    updated layout

--- lib/views/Informasi_Resep.dart
+++ lib/views/Informasi_Resep.dart
@@ -72,33 +72,37 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                       shape: BoxShape.rectangle),
                   padding: EdgeInsets.only(top: 24),
                   child: Column(children: [
-                    Row(children: [
-                      Flexible(
-                          flex: 2,
-                          child: Center(
-                              child: Text("Grilled Salmon",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Quicksand',
-                                  )))),
-                      Flexible(
-                          flex: 2,
-                          child: Center(
-                              child: Text("Rp40.000",
-                                  style: TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Quicksand',
-                                  )))),
-                    ]),
                     Row(
-                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Flexible(
                               flex: 2,
                               child: Container(
-                                  padding: EdgeInsets.only(left: 40),
+                                  padding: EdgeInsets.only(left: 24),
+                                  child: Text("Grilled Salmon",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Quicksand',
+                                      )))),
+                          Flexible(
+                              flex: 2,
+                              child: Container(
+                                  padding: EdgeInsets.only(right: 24),
+                                  child: Text("Rp40.000",
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Quicksand',
+                                      )))),
+                        ]),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                              flex: 2,
+                              child: Container(
+                                  padding: EdgeInsets.only(left: 24),
                                   child: Text(
                                     "Daging dan Sayur",
                                     textAlign: TextAlign.left,
@@ -108,13 +112,13 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                                         fontFamily: 'Quicksand',
                                         color: blue),
                                   ))),
-                          SizedBox(width: 90),
+                          // SizedBox(width: 90),
                           Flexible(
                               flex: 2,
                               child: Container(
-                                  padding: EdgeInsets.only(left: 36),
+                                  padding: EdgeInsets.only(right: 56),
                                   child: Text("/2 Porsi",
-                                      textAlign: TextAlign.right,
+                                      // textAlign: TextAlign.right,
                                       style: TextStyle(
                                         // color: Colors.grey,
                                         fontSize: 12,
@@ -125,7 +129,8 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                     Flexible(
                       flex: 2,
                       child: Container(
-                          margin: EdgeInsets.only(top: 12, left: 0, bottom: 0),
+                          alignment: Alignment.topLeft,
+                          margin: EdgeInsets.only(top: 12, left: 24, bottom: 0),
                           child: Text(
                             "Di samping menjadi salah satu sumber protein yang\nberkualitas tinggi, ikan salmon juga kaya akan asam\nlemak omega-3. Asam lemak omega-3 sangat berguna\nbagi kesehatan jantung, otak, dan organ tubuh lainnya.\nSelain itu rasanya lezat dan mudah diolah.",
                             style: TextStyle(
@@ -136,88 +141,92 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                             // textAlign: TextAlign.justify,
                           )),
                     ),
-                    Flexible(
-                        flex: 3,
-                        child: ListView(
-                          padding:
-                              EdgeInsets.only(top: 12, left: 40, bottom: 0),
-                          children: [
-                            Text("Bahan",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  fontFamily: 'Quicksand',
-                                )),
-                            SizedBox(height: 8),
-                            Text(
-                                "${bullet} 4 potong (@ 200 g) fillet ikan salmon",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} 1 sdt garam",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text(
-                                "${bullet} 1 sdt merica hitam butiran, gerus kasar",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} 2 sdm minyak zaitun",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} 2 sdm mentega",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} 4 siung bawang putih, iris tipis",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} 4 sdm air jeruk lemon",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                          ],
-                        )),
-                    Flexible(
-                        flex: 2,
-                        child: ListView(
-                          padding: EdgeInsets.only(top: 0, left: 40),
-                          children: [
-                            Text("Alat yang dibutuhkan",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  fontFamily: 'Quicksand',
-                                )),
-                            SizedBox(height: 8),
-                            Text("${bullet} Pisau",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} Wajan datar",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12, fontFamily: 'Quicksand',
-                                )),
-                            Text("${bullet} Gelas ukur",
-                                style: TextStyle(
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                  fontFamily: 'Quicksand',
-                                )),
-                          ],
-                        )),
+                    Container(
+                        child: Flexible(
+                            flex: 3,
+                            child: ListView(
+                              padding:
+                                  EdgeInsets.only(top: 0, left: 24, bottom: 0),
+                              children: [
+                                Text("Bahan",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      fontFamily: 'Quicksand',
+                                    )),
+                                SizedBox(height: 8),
+                                Text(
+                                    "${bullet} 4 potong (@ 200 g) fillet ikan salmon",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text("${bullet} 1 sdt garam",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text(
+                                    "${bullet} 1 sdt merica hitam butiran, gerus kasar",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text("${bullet} 2 sdm minyak zaitun",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text("${bullet} 2 sdm mentega",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text(
+                                    "${bullet} 4 siung bawang putih, iris tipis",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                                Text("${bullet} 4 sdm air jeruk lemon",
+                                    style: TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12, fontFamily: 'Quicksand',
+                                    )),
+                              ],
+                            ))),
+                    Container(
+                      child: Flexible(
+                          flex: 2,
+                          child: ListView(
+                            padding: EdgeInsets.only(top: 0, left: 24),
+                            children: [
+                              Text("Alat yang dibutuhkan",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    fontFamily: 'Quicksand',
+                                  )),
+                              SizedBox(height: 8),
+                              Text("${bullet} Pisau",
+                                  style: TextStyle(
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 12, fontFamily: 'Quicksand',
+                                  )),
+                              Text("${bullet} Wajan datar",
+                                  style: TextStyle(
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 12, fontFamily: 'Quicksand',
+                                  )),
+                              Text("${bullet} Gelas ukur",
+                                  style: TextStyle(
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                    fontFamily: 'Quicksand',
+                                  )),
+                            ],
+                          )),
+                    ),
                     Flexible(
                       flex: 1,
                       child: Row(
@@ -226,7 +235,7 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                             Flexible(
                                 flex: 4,
                                 child: Container(
-                                    padding: EdgeInsets.only(left: 40),
+                                    padding: EdgeInsets.only(left: 24),
                                     child: Text("Review",
                                         style: TextStyle(
                                           fontSize: 18,
@@ -237,7 +246,7 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                             Flexible(
                                 flex: 2,
                                 child: Container(
-                                    margin: EdgeInsets.only(right: 40),
+                                    margin: EdgeInsets.only(right: 24),
                                     decoration: BoxDecoration(
                                       color: light_gray,
                                     ),
@@ -262,7 +271,7 @@ class _InformasiResepPageState extends State<InformasiResepPage> {
                       child: Align(
                         alignment: Alignment.center,
                         child: Container(
-                          margin: EdgeInsets.only(top: 18, left: 40, right: 40),
+                          margin: EdgeInsets.only(top: 18, left: 24, right: 24),
                           width: double.infinity,
                           decoration: BoxDecoration(
                             // border: Border.all(color: Colors.transparent),
