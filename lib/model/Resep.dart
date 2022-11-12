d1a80da787bc67d40e72130a3bb883f9e6a7471a
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-11-12 13:45:21 +0700

    required constructor

--- lib/model/Resep.dart
+++ lib/model/Resep.dart
@@ -11,4 +11,17 @@ class Resep {
   List<String>? menuTools;
   String? menuReview;
   List<String>? menuProcedure;
+
+  Resep({
+    required this.image,
+    required this.menuName,
+    required this.menuPortion,
+    required this.menuPrice,
+    required this.menuPreference,
+    required this.menuDescription,
+    required this.menuIngredients,
+    required this.menuTools,
+    required this.menuReview,
+    required this.menuProcedure,
+  });
 }
