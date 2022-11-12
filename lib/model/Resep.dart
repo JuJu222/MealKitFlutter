9acda7811aaa51726e0234655fc26c7dae4a1bff
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-11-12 14:31:18 +0700

    resep one format done

--- lib/model/Resep.dart
+++ lib/model/Resep.dart
@@ -25,3 +25,27 @@ class Resep {
     required this.menuProcedure,
   });
 }
+
+List<Resep> listResep = [
+  Resep(
+      image: 'assets/images/foods/imagefood1.png',
+      menuName: 'Grilled Salmon',
+      menuPortion: '2 Porsi',
+      menuPrice: 'Rp40.000',
+      menuPreference: 'Daging dan Sayur',
+      menuDescription:
+          'Di samping menjadi salah satu sumber protein yang berkualitas tinggi, ikan salmon juga kaya akan asam lemak omega-3. Asam lemak omega-3 sangat berguna bagi kesehatan jantung, otak, dan organ tubuh lainnya. Selain itu rasanya lezat dan mudah diolah.',
+      menuIngredients: [
+        '4 potong (@ 200 g) fillet ikan salmon',
+        '1 sdt garam',
+        '1 sdt merica hitam butiran, gerus kasar',
+        '1 sdt merica hitam butiran, gerus kasar',
+        '2 sdm minyak zaitun',
+        '2 sdm mentega',
+        '4 siung bawang putih, iris tips',
+        '4 sdm air jeruk lemon'
+      ],
+      menuTools: ['Pisau', 'Wajan datar', 'Gelas ukur'],
+      menuReview: '4,8',
+      menuProcedure: ['1. Cuci Semua Sayur', '']),
+];
