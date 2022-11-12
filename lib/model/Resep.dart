400da3869bcde05e2561c273042cefd45884692f
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-11-12 13:31:30 +0700

    class forming

---
+++ lib/model/Resep.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Resep {
+  String? image;
+  String? menuName;
+  String? menuPortion;
+  String? menuPrice;
+  String? menuPreference;
+  String? menuDescription;
+  List<String>? menuIngredients;
+  List<String>? menuTools;
+  String? menuReview;
+  List<String>? menuProcedure;
+}
