45508103aa6bc2d3efd7c441786997380ebe88d3
JuJu222 <japjustin@yahoo.com>
2022-11-15 08:07:28 +0700

    Tidied up UI elements

--- lib/widgets/Card_Resep.dart
+++ lib/widgets/Card_Resep.dart
@@ -14,94 +14,108 @@ class CardResep extends StatelessWidget {
         Navigator.pushNamed(context, InformasiResepPage.routeName,
             arguments: resep);
       },
-      child: Column(
-        // mainAxisAlignment: Alignment.,
-        children: [
-          Expanded(
-            flex: 5,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage("${resep!.menuImage}"),
-                  )),
+      child: Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: Color(0x23000000),
+              blurRadius: 8.0,
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("${resep!.menuName}",
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Quicksand',
-                        color: near_black,
-                      )),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        WidgetSpan(
-                          alignment: PlaceholderAlignment.middle,
-                          child: Icon(Icons.kitchen, size: 10),
-                        ),
-                        TextSpan(
-                          text: "${resep!.menuDuration}",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontFamily: 'Quicksand',
-                            color: near_black,
-                          ),
-                        ),
-                      ],
+          ],
+        ),
+        child: Column(
+          // mainAxisAlignment: Alignment.,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
                     ),
-                  ),
-                ],
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("${resep!.menuImage}"),
+                    )),
               ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
+            Expanded(
+              flex: 2,
+              child: Container(
                 width: double.infinity,
-                height: 12,
-                child: Container(
-                  child: Center(
-                    child: Text("${resep!.menuPrice} /${resep!.menuPortion}",
-                        textAlign: TextAlign.center,
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("${resep!.menuName}",
+                        overflow: TextOverflow.ellipsis,
                         style: TextStyle(
-                          color: Colors.white,
+                          fontSize: 14,
                           fontWeight: FontWeight.bold,
-                          fontSize: 10,
+                          fontFamily: 'Quicksand',
+                          color: near_black,
                         )),
-                  ),
-                  decoration: BoxDecoration(
-                    color: blue,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
+                    const SizedBox(height: 2),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                            alignment: PlaceholderAlignment.middle,
+                            child: Icon(Icons.kitchen, size: 10),
+                          ),
+                          WidgetSpan(
+                              alignment: PlaceholderAlignment.middle,
+                              child: SizedBox(width: 3)),
+                          TextSpan(
+                            text: "${resep!.menuDuration}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontFamily: 'Quicksand',
+                              color: near_black,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                  width: double.infinity,
+                  height: 12,
+                  child: Container(
+                    child: Center(
+                      child: Text("${resep!.menuPrice}/${resep!.menuPortion}",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          )),
+                    ),
+                    decoration: BoxDecoration(
+                      color: blue,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  )
                 // padding: const EdgeInsets.symmetric(
                 // vertical: 12, horizontal: 8),
                 // color: Colors.teal[100],
                 // child: const Text(
                 // "He'd have you all unravel at the"),
-                ),
-          ),
-        ],
-      ),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
